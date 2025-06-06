20ac766d079c928d8025b5d4986e74ddcb7351c0
takhminanabieva <nabieva.t@gmail.com>
2025-06-05 21:10:58 -0500

    Added new nextBaseCRM feature file and implemented Login User Story with two Scenarios: positive and negative

--- src/test/java/com/nextBaseCRM/pages/LoginPage.java
+++ src/test/java/com/nextBaseCRM/pages/LoginPage.java
@@ -1,34 +1,85 @@
 package com.nextBaseCRM.pages;
 
 
+import com.nextBaseCRM.utilities.ConfigurationReader;
 import com.nextBaseCRM.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.text.Utilities;
+import java.time.Duration;
 
 public class LoginPage {
 
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(5));
+    Actions actions = new Actions(Driver.getDriver());
 
-    @FindBy(id="prependedInput")
+    @FindBy(name="USER_LOGIN")
     public WebElement userName;
 
-
-
-    @FindBy(id="prependedInput2")
+    @FindBy(name="USER_PASSWORD")
     public WebElement password;
 
-    @FindBy(name = "_submit")
-    public WebElement submit;
+    @FindBy(xpath = "//input[@type=\"submit\"]")
+    public WebElement login;
+
+    @FindBy(xpath = "//span[.='CRM']")
+    public WebElement crmText;
+
+    @FindBy(xpath = "//div[@class='errortext']")
+    public WebElement errorText;
 
 
-    public void login(String userNameStr, String passwordStr) {
-        userName.sendKeys(userNameStr);
-        password.sendKeys(passwordStr);
-        submit.click();
+    public void enterValidUserName () {
+        wait.until(ExpectedConditions.elementToBeClickable(userName));
+        userName.sendKeys(ConfigurationReader.getProperty("username"));
         // verification that we logged
     }
+
+    public void enterInvalidUserName () {
+        wait.until(ExpectedConditions.elementToBeClickable(userName));
+        userName.sendKeys(("group9@cydeo.com"));
+        // verification that we logged
+    }
+
+
+
+    public void enterValidPassword() {
+        wait.until(ExpectedConditions.elementToBeClickable(password));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
+    }
+
+    public void enterInvalidPassword() {
+        wait.until(ExpectedConditions.elementToBeClickable(password));
+        password.sendKeys("user1");
+    }
+
+    public void clickLoginBtn() {
+        wait.until(ExpectedConditions.elementToBeClickable(login));
+        login.click();
+    }
+
+    public  String loginVerification() {
+        wait.until(ExpectedConditions.visibilityOf(crmText));
+
+        return crmText.getText();
+    }
+
+    public String getErrorText(){
+        wait.until(ExpectedConditions.visibilityOf(errorText));
+        return errorText.getText();
+    }
+
+
+
 
 }
